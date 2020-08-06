5fbfc4fcdf5aff79bbd191d0e078debbabd32961
Chi Nguyen <c.nguyen.prog@gmail.com>
2020-08-07 00:21:34 +0200

    init react app with tailwindcss

---
+++ betting-game-react/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className='min-h-screen min-w-screen bg-gray-900 text-center text-6xl text-gray-200 grid items-center tracking-wider'>
+      AWESOME BETTING APP
+    </div>
+  )
+}
+
+export default App
