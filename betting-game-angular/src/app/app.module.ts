55df31f518b76ba97d98f1b0aa871b65065fc848
Chi Nguyen <c.nguyen.prog@gmail.com>
2020-08-06 23:47:17 +0200

    added tailwindcss

--- betting-game-angular/src/app/app.module.ts
+++ betting-game-angular/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomepageComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
---
+++ betting-game-angular/webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        loader: 'postcss-loader',
+        options: {
+          ident: 'postcss',
+          syntax: 'postcss-scss',
+          plugins: () => [require('postcss-import'), require('tailwindcss'), require('autoprefixer')]
+        }
+      }
+    ]
+  }
+};
