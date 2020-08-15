359074b4d461c2dffc9e2d2d393ae286a4b6b78a
Chi Nguyen <c.nguyen.prog@gmail.com>
2020-08-15 23:28:32 +0200

    added vue-i18n

--- betting-game-vue/src/main.ts
+++ betting-game-vue/src/main.ts
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import i18n from './i18n'
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  i18n,
   router
 }).$mount('#app')
