259d1c154220c2e57bfeddc4b3215bc82a2e5e4a
Marcel Gauß <Marcel_Gauss@icloud.com>
2020-10-04 21:39:27 +0200

    remove unused import in navbar

--- betting-game-react/src/components/Navbar/Navbar.tsx
+++ betting-game-react/src/components/Navbar/Navbar.tsx
@@ -1,11 +1,5 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Redirect,
-  Route,
-  RouteProps,
-  Switch,
-} from 'react-router-dom';
+import { Redirect, Route, RouteProps, Switch } from 'react-router-dom';
 
 import routes, { MATCHES } from '../../config/routes';
 import NavbarButton from '../NavbarButton/NavbarButton';
