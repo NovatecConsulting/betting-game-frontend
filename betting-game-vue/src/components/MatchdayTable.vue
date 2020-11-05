47e33ed4f531cbaa910f92b8acfa3eae5e3950fe
Chi Nguyen <c.nguyen.prog@gmail.com>
2020-11-05 17:32:04 +0100

    added pulsing matchday, updated theme

--- betting-game-vue/src/components/MatchdayTable.vue
+++ betting-game-vue/src/components/MatchdayTable.vue
@@ -1,12 +1,60 @@
 <template>
   <!-- TODO MOBILE VIEW -->
-  <div class="matchday min-w-sm max-w-3xl ">
-    <div class="matchday__loading flex justify-center items-center py-64" v-if="!matchday">
-      <Loading />
-    </div>
-    <div class="px-4 sm:px-8 py-4 overflow-x-auto" v-else>
+  <div class="matchday min-w-sm">
+    <div class="matchday__pulse-table sm:px-8 md:py-4 overflow-x-auto" v-if="!matchday">
       <div class="inline-block min-w-full shadow-lg rounded-lg overflow-hidden">
-        <table class="matchday__table leading-normal w-full table-auto bg-white">
+        <table class="matchday__table leading-normal w-full table-auto bg-background">
+          <thead>
+            <tr>
+              <th
+                colspan="4"
+                class="px-8 py-5 border-b-2 border-gray-200 bg-gray-100 text-left text-sm font-semibold text-gray-600 uppercase tracking-wider"
+              >
+                Matchday
+              </th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr
+              v-for="match in [0, 1, 2, 3, 4, 5, 6, 7, 8]"
+              :key="match"
+              class="matchday__row hover:bg-gray-100 cursor-pointer"
+            >
+              <td class="matchday__row-item">
+                <div class="flex items-center">
+                  <div class="flex-shrink-0 w-10 h-10">
+                    <div class="rounded-full bg-gray-400 h-10 w-10"></div>
+                  </div>
+                  <div class="flex-1 space-y-4 py-1">
+                    <div class="ml-3 h-4 bg-gray-400 rounded w-24"></div>
+                  </div>
+                </div>
+              </td>
+              <td class="matchday__row-item">
+                <div class="flex items-center">
+                  <div class="flex-shrink-0 w-10 h-10">
+                    <div class="rounded-full bg-gray-400 h-10 w-10"></div>
+                  </div>
+                  <div class="flex-1 space-y-4 py-1">
+                    <div class="ml-3 h-4 bg-gray-400 rounded w-24"></div>
+                  </div>
+                </div>
+              </td>
+              <td class="matchday__row-item">
+                <div class="flex-1 space-y-4 py-1">
+                  <div class="h-4 bg-gray-400 rounded w-24"></div>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+
+    <div class="matchday__table sm:px-8 md:py-4 overflow-x-auto" v-else>
+      <div class="inline-block shadow-lg rounded-lg overflow-hidden">
+        <table class="matchday__table leading-normal table-auto max-w-lg mx-auto bg-background">
           <thead>
             <tr>
               <th
@@ -17,6 +65,26 @@
                 {{ matchday.firstMatchStartDateTime | dateTime }}
                 -
                 {{ matchday.lastMatchStartDateTime | dateTime }}
+                <button
+                  class="float-right cursor-pointer"
+                  :class="{ 'animate-spin': matchdayIsLoading }"
+                  v-on:click.exact="getMatchday()"
+                >
+                  <svg
+                    class="w-5 h-5"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
+                    ></path>
+                  </svg>
+                </button>
               </th>
             </tr>
           </thead>
@@ -27,31 +95,32 @@
               :key="match.id"
               class="matchday__row hover:bg-gray-100 cursor-pointer"
             >
-              <td class="matchday__row-item">
-                <div class="flex items-center">
-                  <div class="flex-shrink-0 w-10 h-10">
-                    <img class="w-full h-full rounded-full" :src="match.home.logo" alt="" />
+              <td class="matchday__col" colspan="2">
+                <div class="matchday__match flex flex-col md:flex-row max-w-screenw-1/2 truncate">
+                  <div class="matchday__team-home flex items-center w-32 md:w-64 ">
+                    <div class="flex-shrink-0 w-10 h-10">
+                      <img class="w-full h-full rounded-full" :src="match.home.logo" alt="" />
+                    </div>
+                    <div class="ml-3">
+                      <p class="text-gray-900 whitespace-no-wrap">
+                        {{ match.home.name }}
+                      </p>
+                    </div>
                   </div>
-                  <div class="ml-3">
-                    <p class="text-gray-900 whitespace-no-wrap">
-                      {{ match.home.name }}
-                    </p>
+
+                  <div class="matchday__team-guest flex items-center mt-2 md:mt-0">
+                    <div class="flex-shrink-0 w-10 h-10">
+                      <img class="w-full h-full rounded-full" :src="match.guest.logo" alt="" />
+                    </div>
+                    <div class="ml-3">
+                      <p class="text-gray-900 whitespace-no-wrap">
+                        {{ match.guest.name }}
+                      </p>
+                    </div>
                   </div>
                 </div>
               </td>
-              <td class="matchday__row-item">
-                <div class="flex items-center">
-                  <div class="flex-shrink-0 w-10 h-10">
-                    <img class="w-full h-full rounded-full" :src="match.guest.logo" alt="" />
-                  </div>
-                  <div class="ml-3">
-                    <p class="text-gray-900 whitespace-no-wrap">
-                      {{ match.guest.name }}
-                    </p>
-                  </div>
-                </div>
-              </td>
-              <td class="matchday__row-item" :class="{ 'text-base font-bold': match.result }">
+              <td class="matchday__col" :class="{ 'text-base font-bold': match.result }">
                 <p class="text-gray-900">
                   <span v-if="match.result">{{
                     match.result.final.goalsHome + ' : ' + match.result.final.goalsGuest
@@ -82,14 +151,22 @@ export default class MatchdayTable extends Vue {
   created() {
     this.$store.dispatch('getCurrentMatchday')
   }
+
+  getMatchday() {
+    this.$store.dispatch('getCurrentMatchday')
+  }
+
   get matchday(): Matchday | null {
     return this.matchdayStore.currentMatchday
+  }
+  get matchdayIsLoading(): boolean {
+    return this.matchdayStore.isLoading
   }
 }
 </script>
 
 <style scoped>
-.matchday__row-item {
+.matchday__col {
   @apply px-5 py-4 border-b border-gray-200 text-sm;
 }
 </style>
